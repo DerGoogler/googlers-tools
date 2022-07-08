a7e5c418499efa2ac5a065eea41042f683a87cbb
Der_Googler <jimmyboehm@dergoogler.com>
2022-07-08 22:45:30 +0200

    -

--- src/utils/Link.ts
+++ src/utils/Link.ts
@@ -7,10 +7,10 @@ namespace Link {
   export function validURL(input: string): boolean {
     var pattern = new RegExp(
       "^(https?:\\/\\/)?" + // protocol
-        "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
-        "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
-        "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
-        "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+      "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+      "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
         "(\\#[-a-z\\d_]*)?$",
       "i"
     ); // fragment locator
@@ -38,6 +38,41 @@ namespace Link {
    */
   export function getSubPath(url: string): string {
     return window.location.href.replace(/(\?(.*?)=(.*)|\?)/gm, "") + url;
+  }
+
+  export function parseURLParams(url: string): string[] | undefined {
+    var queryStart = url.indexOf("?") + 1,
+      queryEnd = url.indexOf("#") + 1 || url.length + 1,
+      query = url.slice(queryStart, queryEnd - 1),
+      pairs = query.replace(/\+/g, " ").split("&"),
+      parms: any = {},
+      i,
+      n,
+      v,
+      nv;
+
+    if (query === url || query === "") return;
+
+    for (i = 0; i < pairs.length; i++) {
+      nv = pairs[i].split("=", 2);
+      n = decodeURIComponent(nv[0]);
+      v = decodeURIComponent(nv[1]);
+
+      if (!parms.hasOwnProperty(n)) parms[n] = [];
+      parms[n].push(nv.length === 2 ? v : null);
+    }
+    return parms;
+  }
+
+  export function request<T = any>(url: string, callback: (status: XMLHttpRequest["status"], data: T) => void) {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+      if (this.readyState == 4) {
+        callback(xhr.status, xhr.response);
+      }
+    };
+    xhr.open("GET", url, true);
+    xhr.send();
   }
 }
 
