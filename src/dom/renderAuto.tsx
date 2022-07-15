56fa5bbc39ea818154373387faceea044d558d26
Der_Googler <jimmyboehm@dergoogler.com>
2022-07-15 21:25:02 +0200

    Allow render functional components

--- src/dom/renderAuto.tsx
+++ src/dom/renderAuto.tsx
@@ -5,7 +5,7 @@ import { createRoot } from "react-dom/client";
  * React DOM render method to render the DOM and root element automatically. Requires React 18+ and an component with call syntax.
  * @param InitComponent {ComponentClass} Uses the given component to render the DOM and the required HTML root element
  */
-export default function(InitComponent: React.ComponentClass): void {
+export default function(InitComponent: React.ElementType): void {
   const app = document.createElement(InitComponent.constructor.name!);
   document.body.prepend(app);
   const container = document.querySelector<Element>(InitComponent.constructor.name!);
