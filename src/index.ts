65ac4b2685d3f6b435ca7d2e337569ab8defa8c5
Der_Googler <jimmyboehm@dergoogler.com>
2022-07-31 00:33:16 +0200

    add JR

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import dom from "./dom";
+import JR from "./JR";
 import link from "./link";
 import util from "./util";
 import BSON from "./utils/BSON";
@@ -6,7 +7,7 @@ import Logger, { ILogger, ILoggerStatic, ILoggerStaticReturn } from "./utils/Log
 import SharedPreferences, { ISharedPreferences } from "./utils/SharedPreferences";
 
 // Main
-export { util, dom, link };
+export { util, dom, link, JR };
 
 // Logger
 export { Logger, ILogger, ILoggerStatic, ILoggerStaticReturn };
